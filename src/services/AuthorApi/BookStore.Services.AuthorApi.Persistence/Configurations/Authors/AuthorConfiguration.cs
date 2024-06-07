9a0efff50b47740e122a96cb1b557c03942839cd
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2024-06-07 11:12:54 +0200

    Implemented Author service persistence project

---
+++ src/services/AuthorApi/BookStore.Services.AuthorApi.Persistence/Configurations/Authors/AuthorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Services.AuthorApi.Domain.Authors;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.Services.AuthorApi.Persistence.Configurations.Authors;
+
+internal class AuthorConfiguration : EntityConfiguration<Author>
+{
+    public override void Configure(EntityTypeBuilder<Author> builder)
+    {
+        base
+            .Configure(
+                builder
+            );
+    }
+}
