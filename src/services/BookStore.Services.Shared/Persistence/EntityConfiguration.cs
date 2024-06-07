5ad56989487a80897837b7b51fdeef9b7a790a4b
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2024-06-07 11:12:06 +0200

    Implemented EntityConfiguration class

---
+++ src/services/BookStore.Services.Shared/Persistence/EntityConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Services.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.Services.Shared.Persistence;
+
+public class EntityConfiguration<T> : IEntityTypeConfiguration<T> where T : Entity
+{
+    public virtual void Configure(
+        EntityTypeBuilder<T> builder
+    )
+    {
+        builder.ToTable(typeof(T).Name);
+
+        builder.Property(x =>
+                x.IsEnabled
+            )
+            .IsRequired()
+            .HasDefaultValue(
+                true
+            )
+            .ValueGeneratedNever();
+
+        builder
+            .Property(x =>
+                x.CreatedAt
+            )
+            .HasDefaultValueSql(
+                "GETUTCDATE()"
+            );
+        
+        builder
+            .Property(x => 
+                x.UpdatedAt
+            )
+            .HasDefaultValueSql(
+                "GETUTCDATE()"
+            )
+            .IsConcurrencyToken();
+    }
+}
