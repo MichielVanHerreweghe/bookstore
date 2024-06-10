cf79061c16a19dc9709ca4eb7e9a8650c24487b7
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2024-06-10 23:34:09 +0200

    Implemented book service services project

---
+++ src/services/BookApi/BookStore.Services.BookApi.Shared/Authors/IAuthorService.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Services.BookApi.Domain.Authors;
+
+namespace BookStore.Services.BookApi.Shared.Authors;
+
+public interface IAuthorService
+{
+    public Task<int> CreateAsync(
+        Author model,
+        CancellationToken cancellationToken
+    );
+
+    public Task<IEnumerable<Author>> GetAllAsync(
+        CancellationToken cancellationToken
+    );
+
+    public Task<Author> GetByIdAsync(
+        int id,
+        CancellationToken cancellationToken
+    );
+
+    public Task UpdateByIdAsync(
+        int id,
+        Author model,
+        CancellationToken cancellationToken
+    );
+
+    public Task DeleteByIdAsync(
+        int id,
+        CancellationToken cancellationToken
+    );
+}
