719c6f9f0402f17b4280c49432cdd24360c1aac2
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2024-06-10 23:13:11 +0200

    Added AuthorId for coupling with Author service

--- src/services/BookApi/BookStore.Services.BookApi.Domain/Authors/Author.cs
+++ src/services/BookApi/BookStore.Services.BookApi.Domain/Authors/Author.cs
@@ -4,14 +4,23 @@ namespace BookStore.Services.BookApi.Domain.Authors;
 
 public class Author : Entity
 {
+    public int AuthorId { get; private set; }
     public string Name { get; private set; } = default!;
 
     private Author() { }
 
     public Author(
+        int authorId,
         string name
     )
     {
+        AuthorId = Guard
+            .Against
+            .NegativeOrZero(
+                authorId,
+                nameof(authorId)
+            );
+
         Name = Guard
             .Against
             .NullOrWhiteSpace(
