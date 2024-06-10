c87f3d77e65bcb247b8346893570321e67847814
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2024-06-10 22:51:55 +0200

    Implemented book service domain project

---
+++ src/services/BookApi/BookStore.Services.BookApi.Domain/Authors/Author.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Services.BookApi.Domain.Books;
+
+namespace BookStore.Services.BookApi.Domain.Authors;
+
+public class Author : Entity
+{
+    public string Name { get; private set; } = default!;
+
+    private Author() { }
+
+    public Author(
+        string name
+    )
+    {
+        Name = Guard
+            .Against
+            .NullOrWhiteSpace(
+                name,
+                nameof(name)
+        );
+    }
+
+    public void Update(
+        string name
+    )
+    {
+        UpdateName(
+            name
+        );
+    }
+
+    private void UpdateName(
+        string name
+    )
+    {
+        Name = Guard
+            .Against
+            .NullOrWhiteSpace(
+                name,
+                nameof(name)
+            );
+    }
+}
