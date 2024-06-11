42b84df8c3dfedc534bb6fb2d98e7440d8af9a50
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2024-06-11 08:50:19 +0200

    Implemented dtos, requests and responses

---
+++ src/services/BookApi/BookStore.Services.BookApi.Shared/Authors/AuthorDto.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStore.Services.BookApi.Shared.Authors;
+
+public abstract class AuthorDto
+{
+    public record Index(
+        int Id,
+        int AuthorId,
+        string Name
+    );
+
+    public record Detail(
+        int Id,
+        int AuthorId,
+        string Name
+    );
+
+    public record Mutate(
+        int AuthorId,
+        string Name  
+    );
+}
---
+++ src/services/BookApi/BookStore.Services.BookApi.Shared/Books/BookResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace BookStore.Services.BookApi.Shared.Books;
+
+public abstract class BookResponse
+{
+    public record Index(
+        IReadOnlyCollection<BookDto.Index> Books  
+    );
+
+    public record Detail(
+        BookDto.Detail Book  
+    );
+
+    public record Create(
+        int Id  
+    );
+}
